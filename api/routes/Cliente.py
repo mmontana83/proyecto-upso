1cbb83f0bb5bf16de7cc853c589b0acf6db22e8e
Martín Montaña <martin.miguel.montana@gmail.com>
2023-10-30 22:00:11 -0300

    Se agrega rutina para actualizar clientes

--- api/routes/Cliente.py
+++ api/routes/Cliente.py
@@ -21,7 +21,7 @@ def get_clientes_by_usuario(id_usuario):
         return jsonify({'mensaje': 'Error'})
     
 #Obtengo los datos de un cliente en particular
-@app.route('/usuario/<id_usuario>/clientes/<id_cliente>', methods = ['GET'])
+@app.route('/usuario/<id_usuario>/cliente/<id_cliente>', methods = ['GET'])
 def get_cliente_by_id_cliente(id_usuario, id_cliente):
     try:
         cur = mysql.connection.cursor()
@@ -38,12 +38,31 @@ def get_cliente_by_id_cliente(id_usuario, id_cliente):
 @app.route('/usuario/<id_usuario>/nuevocliente/', methods = ['POST'])
 def registrar_cliente(id_usuario):
     try:
+        cur = mysql.connection.cursor()
+        cur.callproc('sp_obtenerClienteByID', [id_usuario, request.json['id_cliente']])
+        fila = cur.fetchone()
+
+        if fila == None:
+            cur.callproc('sp_insertarCliente',[id_usuario, request.json['nombre'], request.json['apellido'], 
+                                            request.json['empresa'], request.json['email'], request.json['telefono'],
+                                            request.json['direccion'], request.json['id_tipoCondicionIVA'], request.json['id_cliente']])
+            mysql.connection.commit() #Esto confirma la acción de inserción
+            return jsonify({'mensaje':'Cliente Registrado con Éxito'})
+        else:
+            return jsonify({'mensaje':'Cliente ya se encuentra Registrado'})
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error'})
+    
+#Actualizar Cliente
+@app.route('/usuario/<id_usuario>/cliente/<id_cliente>', methods = ['POST'])
+def actualizar_cliente(id_usuario, id_cliente):
+    try:
         print(request.json)
         cur = mysql.connection.cursor()
-        cur.callproc('sp_insertarCliente',[request.json['id_cliente'], request.json['nombre'], request.json['apellido'], 
+        cur.callproc('sp_actualizarCliente',[id_usuario, request.json['nombre'], request.json['apellido'], 
                                            request.json['empresa'], request.json['email'], request.json['telefono'],
-                                           request.json['direccion'], request.json['id_tipoCondicionIVA'], id_usuario])
+                                           request.json['direccion'], request.json['id_tipoCondicionIVA'], id_cliente])
         mysql.connection.commit() #Esto confirma la acción de inserción
-        return jsonify({'mensaje':'Cliente Registrado'})
+        return jsonify({'mensaje':'Cliente Actualizado con Éxito'})
     except Exception as ex:
         return jsonify({'mensaje': 'Error'})
