0e8eb2d89d2bde7089aae29f0f7d33c6a7ccecd5
Martín Montaña <martin.miguel.montana@gmail.com>
2023-11-01 21:31:57 -0300

    Correción del comentario para la conexión a la base de datos

--- backend/api/db/db.py
+++ backend/api/db/db.py
@@ -1,7 +1,8 @@
 from api import app
 from flask_mysqldb import MySQL
 
-app.config['MYSQL_HOST'] = 'casitamontana.ddns.net' #cambiarlo por casitamontana.ddnet.
+#cambiarlo por localhost si es instancia local o casitamontana.ddns.net si se conecta al servidor mysql
+app.config['MYSQL_HOST'] = 'casitamontana.ddns.net' 
 app.config['MYSQL_USER'] = 'proyecto'
 app.config['MYSQL_PASSWORD'] ='proyecto'
 app.config['MYSQL_DB'] = 'proyecto'
