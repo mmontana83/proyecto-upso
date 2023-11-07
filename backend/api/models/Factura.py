859220cf4c542e19e7568cf11e6371f81f584df4
Martín Montaña <martin.miguel.montana@gmail.com>
2023-11-07 15:33:05 -0300

    Implementación del método Insertar Factura

--- backend/api/models/Factura.py
+++ backend/api/models/Factura.py
@@ -20,7 +20,32 @@ class Factura():
             'id_condicionVenta': self._id_condicionVenta,
             'id_usuario': self._id_usuario
         }
+    @staticmethod
+    def insertarFactura(jsonFactura, jsonDetalleFactura):
+        try:
+            factura = Factura(jsonFactura)
+            detalleFactura = DetalleFactura(jsonDetalleFactura)
 
+            #Inserto el encabezado de la factura
+            cur = mysql.connection.cursor()
+            cur.callproc('sp_insertarFactura', [factura._fecha, factura._total, factura._id_tipoFactura, factura._id_cliente,
+                                                factura._id_condicionVenta, factura._id_usuario])
+            mysql.connection.commit()
+            cur.close()
+
+            #Inserto el Detalle de la Factura
+            cur = mysql.connection.cursor()
+            for fila in jsonDetalleFactura:
+                cur.callproc('sp_insertarFacturaDetalle', [fila._id_producto, fila._cantidad, fila._precio])
+                mysql.connection.commit()
+            cur.close()
+
+        except Exception as ex:
+            return {'mensaje':str(ex)}
+        
+    @staticmethod
+    def obtenerFacturasById_Cliente(id_cliente):
+        None
 
 class DetalleFactura():
     
@@ -29,13 +54,11 @@ class DetalleFactura():
         self._id_producto = json['id_producto']
         self._cantidad = json['cantidad']
         self._precio = json['precio']        
-        self._precioTotal = json['precioTotal']
     
     def to_json(self):
         return{
             'id_factura': self._id_factura,
             'id_producto': self._id_producto,
             'cantidad': self._cantidad,
-            'precio': self._precio,
-            'precioTotal': self._precioTotal
+            'precio': self._precio
         }
